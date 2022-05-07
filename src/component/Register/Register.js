9be4b7488ce925f42754d2f7fe01c7e8c8941ba5
Shaila Nasrin <drshaila2021@gmail.com>
2022-05-07 10:58:21 -0500

    create user authintication

--- src/component/Register/Register.js
+++ src/component/Register/Register.js
@@ -1,14 +1,51 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import {
+  useAuthState,
+  useCreateUserWithEmailAndPassword,
+} from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
+import Loading from "../Loading/Loading";
 
 const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [createUserWithEmailAndPassword, user, loading, error] =
+    useCreateUserWithEmailAndPassword(auth);
+  const [user1, loading1, error1] = useAuthState(auth);
+  const navigate = useNavigate();
+
+  const handleEmailBlur = (event) => {
+    setEmail(event.target.value);
+  };
+  const handlePasswordBlur = (event) => {
+    setPassword(event.target.value);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    createUserWithEmailAndPassword(email, password);
+    console.log(email, password);
+  };
+
+  if (user1) {
+    navigate("/");
+  }
+
+  if (loading1) {
+    return <Loading></Loading>;
+  }
+
   return (
     <div>
-      <form className="w-50 mx-auto d-block my-5 border p-5 rounded-4 shadow-sm bg-body ">
+      <form
+        onSubmit={handleSubmit}
+        className="w-50 mx-auto d-block my-5 border p-5 rounded-4 shadow-sm bg-body "
+      >
         <h4 className="text-center text-secondary">Register Now..</h4>
 
         <div className="mb-3">
           <input
+            onBlur={handleEmailBlur}
             type="email"
             name="email"
             className="form-control"
@@ -19,6 +56,7 @@ const Register = () => {
 
         <div className="mb-3">
           <input
+            onBlur={handlePasswordBlur}
             type="password"
             name="password"
             className="form-control"
